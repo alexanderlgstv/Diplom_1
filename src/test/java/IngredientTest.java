24b42aa48ec7587d6438632162469b9e7743bca7
alexanderlgstv <shoood666@yandex.ru>
2023-06-06 22:52:11 +0700

    first commit add files

---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,52 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runner.RunWith;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+
+@RunWith(Parameterized.class)
+public class IngredientTest {
+
+    private Ingredient ingredient;
+
+    private final IngredientType ingredientType;
+    private final String name;
+    private final float price;
+
+    public IngredientTest(IngredientType ingredientType, String name, float price) {
+        this.ingredientType = ingredientType;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getParameters() {
+        return new Object[][] {
+                {IngredientType.SAUCE, "hot sauce", 100},
+                {IngredientType.FILLING, "cutlet", 100}
+        };
+    }
+
+    @Before
+    public void setUp() {
+        ingredient = new Ingredient(ingredientType, name, price);
+    }
+
+    @Test
+    public void getNameTest() {
+        Assert.assertEquals(name, ingredient.getName());
+    }
+
+    @Test
+    public void getPriceTest() {
+        Assert.assertEquals(price, ingredient.getPrice(), 0);
+    }
+
+    @Test
+    public void getTypeTest() {
+        Assert.assertEquals(ingredientType, ingredient.getType());
+    }
+}
